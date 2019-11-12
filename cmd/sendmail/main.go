36ca5330d87d4306a6d6dd075447f936a93418bc
Ivan Daunis <ivan.daunis@retailnext.net>
2019-11-12 12:11:31 -0800

    Improve error messaging

--- cmd/sendmail/main.go
+++ cmd/sendmail/main.go
@@ -1,8 +1,6 @@
 // Copyright (c) 2019, RetailNext, Inc.
-// This material contains trade secrets and confidential information of
-// RetailNext, Inc.  Any use, reproduction, disclosure or dissemination
-// is strictly prohibited without the explicit written permission
-// of RetailNext, Inc.
+// This software may be modified and distributed under the terms
+// of the BSD license. See the LICENSE file for details.
 // All rights reserved.
 
 package main
@@ -10,29 +8,42 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 
 	sendmail "github.com/retailnext/sentry-sendmail"
 )
 
 func main() {
+	appName := filepath.Base(os.Args[0]) + ":"
+
 	sendmail.ParseOptions()
 	err := sendmail.SentryConfig()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(appName, "error:", err)
 		return
 	}
 
-	headers, body := sendmail.ReadData(bufio.NewReader(os.Stdin))
+	headers, body, rawData := sendmail.ReadData(bufio.NewReader(os.Stdin))
 	message, err := sendmail.BuildMessage(headers, body)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(appName, "error:", err)
 		return
 	}
 
 	err = sendmail.SentrySend(message, headers)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(appName, "error:", err)
+		queueDir := "/var/spool/sentry-sendmail/queue"
+		os.MkdirAll(queueDir, os.ModePerm)
+		logFile, err := ioutil.TempFile(queueDir, "")
+		if err != nil {
+			fmt.Println(appName, "error: Can not save envelope:", err)
+			return
+		}
+		fmt.Fprint(logFile, rawData)
+		fmt.Println(appName, "info: Original envelope saved on:", queueDir)
 		return
 	}
 }
