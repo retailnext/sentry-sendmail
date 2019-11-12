2ae92ce73007094fe02375843a670d9227fddebc
Ivan Daunis <ivan.daunis@retailnext.net>
2019-11-12 15:45:52 -0800

    Log SentrySend error onto journal if available

--- cmd/sendmail/main.go
+++ cmd/sendmail/main.go
@@ -7,43 +7,51 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
+
+	"github.com/coreos/go-systemd/v22/journal"
 
 	sendmail "github.com/retailnext/sentry-sendmail"
 )
 
+var (
+	appName string
+)
+
+func wrap(err error) string {
+	return fmt.Sprintf("%s: error: %s", appName, err.Error())
+}
+
 func main() {
-	appName := filepath.Base(os.Args[0]) + ":"
+	appName = filepath.Base(os.Args[0])
 
 	sendmail.ParseOptions()
 	err := sendmail.SentryConfig()
 	if err != nil {
-		fmt.Println(appName, "error:", err)
+		fmt.Fprintln(os.Stderr, wrap(err))
 		return
 	}
 
 	headers, body, rawData := sendmail.ReadData(bufio.NewReader(os.Stdin))
 	message, err := sendmail.BuildMessage(headers, body)
 	if err != nil {
-		fmt.Println(appName, "error:", err)
+		fmt.Fprintln(os.Stderr, wrap(err))
 		return
 	}
 
 	err = sendmail.SentrySend(message, headers)
 	if err != nil {
-		fmt.Println(appName, "error:", err)
-		queueDir := "/var/spool/sentry-sendmail/queue"
-		os.MkdirAll(queueDir, os.ModePerm)
-		logFile, err := ioutil.TempFile(queueDir, "")
-		if err != nil {
-			fmt.Println(appName, "error: Can not save envelope:", err)
-			return
+		if journal.Enabled() {
+			vars := map[string]string{}
+			journal.Send(wrap(err), journal.PriErr, vars)
+			journal.Send(rawData, journal.PriErr, vars)
+		} else {
+			bytes, _ := json.Marshal(map[string]string{"message": rawData})
+			fmt.Fprintln(os.Stderr, wrap(err), string(bytes))
 		}
-		fmt.Fprint(logFile, rawData)
-		fmt.Println(appName, "info: Original envelope saved on:", queueDir)
 		return
 	}
 }
